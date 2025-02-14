0e98efad689da21ac47ef912d22b64daf41b89cc
seok-young <nsy7160@gmail.com>
2025-02-14 09:58:07 +0900

    append()

--- src/main/java/com/ll/SimpleDb/Sql.java
+++ src/main/java/com/ll/SimpleDb/Sql.java
@@ -1,29 +1,35 @@
 package com.ll.SimpleDb;
 
-import java.util.List;
+import java.util.ArrayList;
+import java.util.HashMap;
 
 public class Sql {
-    public String querypart;
-    public List<Object> params;
+    private final ArrayList<HashMap<String, Object>> sqlParts;
 
     public Sql() {
-        this.querypart = "";
-        this.params = null;
+        this.sqlParts = new ArrayList<>();
     }
 
-    public Sql append(String sqlpart) {
-
-        return null;
+    public Sql append(String query) {
+        return append(query, null); // 중복 코드 줄이기
     }
 
-    public Sql append(String sqlpart, Object... params) {
-        return null;
+    public Sql append(String query, Object param) {
+        HashMap<String, Object> sqlPart = new HashMap<>();
+        sqlPart.put("querypart", query);
+        sqlPart.put("params", param);
+        sqlParts.add(sqlPart);
+        return this; // 체이닝을 위해 this 반환
     }
 
-    public Long insert() {
-        return null;
+    @Override
+    public String toString() {
+//        return "Sql{" +
+//                "sqlParts=" + sqlParts +
+//                '}';
+        return sqlParts.toString();
     }
-}
+    }
 
 
 
